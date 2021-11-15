c355556aea8593a19d350274c8861de839dbd4ea
iblanc231 <baconsoap323@gmail.com>
2021-11-14 21:17:57 -0500

    Initial commit

---
+++ src/main/java/net/iblanc/practicemod/mixin/PracticeMixin.java
@@ -0,0 +1,18 @@
+package net.iblanc.practicemod.mixin;
+
+import net.iblanc.practicemod.PracticeMod;
+import net.minecraft.client.gui.screen.TitleScreen;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(TitleScreen.class)
+public class PracticeMixin {
+
+	@Inject(at = @At("HEAD"), method = "init()V")
+	private void init(CallbackInfo info) {
+		PracticeMod.LOGGER.info("This line is printed by an example mod mixin!");
+	}
+
+}
